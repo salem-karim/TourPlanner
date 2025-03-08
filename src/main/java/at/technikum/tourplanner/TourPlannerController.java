727f4a418d7db7dfad1e828f82fdb130aaa60e4b
Felix Hadinger <felix.hadinger.2802@gmail.com>
2025-03-08 14:41:18 +0100

    added a main_window.fxml with basic gui elements

--- src/main/java/at/technikum/tourplanner/TourPlannerController.java
+++ src/main/java/at/technikum/tourplanner/TourPlannerController.java
@@ -3,7 +3,7 @@ package at.technikum.tourplanner;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class TourPlannerController {
   @FXML
   private Label welcomeText;
 
