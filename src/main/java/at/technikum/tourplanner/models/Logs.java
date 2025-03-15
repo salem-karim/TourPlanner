241546462ccdee8099d79eebf2fe828c9cae50cf
salem-karim <salem.karim@icloud.com>
2025-03-15 14:50:50 +0100

    User Lomboks AllArgsConstructor

--- src/main/java/at/technikum/tourplanner/models/Logs.java
+++ src/main/java/at/technikum/tourplanner/models/Logs.java
@@ -1,10 +1,12 @@
 package at.technikum.tourplanner.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Logs {
     private int id;
     private String date_time;
@@ -14,15 +16,6 @@ public class Logs {
     private int total_time;
     private int rating;
 
-    public Logs(int id, String date_time, String comment, int difficulty, int total_distance, int total_time, int rating) {
-        this.id = id;
-        this.date_time = date_time;
-        this.comment = comment;
-        this.difficulty = difficulty;
-        this.total_distance = total_distance;
-        this.total_time = total_time;
-        this.rating = rating;
-    }
 }
 
 
--- src/main/java/at/technikum/tourplanner/models/Tour.java
+++ src/main/java/at/technikum/tourplanner/models/Tour.java
@@ -1,11 +1,14 @@
 package at.technikum.tourplanner.models;
 
 import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Tour {
     private UUID id;
     private String name;
@@ -16,18 +19,5 @@ public class Tour {
     private int tour_distance;
     private int estimated_time;
     private String route_info; //image???
-
-    public Tour(UUID id, String name, String tour_description, String from, String to, String transport_type, int tour_distance, int estimated_time, String route_info) {
-        this.id = id;
-        this.name = name;
-        this.tour_description = tour_description;
-        this.from = from;
-        this.to = to;
-        this.transport_type = transport_type;
-        this.tour_distance = tour_distance;
-        this.estimated_time = estimated_time;
-        this.route_info = route_info;
-    }
-
 
 }
