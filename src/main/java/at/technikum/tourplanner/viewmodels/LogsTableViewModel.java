6e7fd02c62e6ebafaaf6b6f9ae1a51f5e9a89f10
Felix Hadinger <felix.hadinger.2802@gmail.com>
2025-03-15 15:08:11 +0100

    added all nessesary viewmodels and added some code

---
+++ src/main/java/at/technikum/tourplanner/viewmodels/LogsTableViewModel.java
@@ -0,0 +1,5 @@
+package at.technikum.tourplanner.viewmodels;
+
+// responsible for tour_logs.fxml (listing of the logs info) !!
+public class LogsTableViewModel {
+}
--- src/main/java/at/technikum/tourplanner/viewmodels/TourTableViewModel.java
+++ src/main/java/at/technikum/tourplanner/viewmodels/TourTableViewModel.java
@@ -1,103 +1,42 @@
 package at.technikum.tourplanner.viewmodels;
 
 import at.technikum.tourplanner.models.Tour;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.util.UUID;
 
-// responsible for tour_general.fxml !!
 public class TourTableViewModel {
+    private final ObservableList<TourViewModel> data =
+            FXCollections.observableArrayList(
+                    new TourViewModel(new Tour(UUID.randomUUID(), "Kahlsberg Wanderung", "Schöne Wanderung aufm Kahlsberg", "Nußdorf", "Kahlsberg", "zu Fuß", 5, 2, "img")),
+                    new TourViewModel(new Tour(UUID.randomUUID(), "Kahlsberg Wanderung", "Schöne Wanderung aufm Kahlsberg", "Nußdorf", "Kahlsberg", "zu Fuß", 5, 2, "img"))
 
-    private final StringProperty id;
-    private final StringProperty name;
-    private final StringProperty tour_description;
-    private final StringProperty from;
-    private final StringProperty to;
-    private final StringProperty transport_type;
-    private final IntegerProperty tour_distance;
-    private final IntegerProperty estimated_time;
-    private final StringProperty route_info;
+                    );
 
-
-    public TourTableViewModel(Tour tour) {
-        this.id = new SimpleStringProperty(tour.getId().toString());
-        this.name = new SimpleStringProperty(tour.getName());
-        this.tour_description = new SimpleStringProperty(tour.getTour_description());
-        this.from = new SimpleStringProperty(tour.getFrom());
-        this.to = new SimpleStringProperty(tour.getTo());
-        this.transport_type = new SimpleStringProperty(tour.getTransport_type());
-        this.tour_distance = new SimpleIntegerProperty(tour.getTour_distance());
-        this.estimated_time = new SimpleIntegerProperty(tour.getEstimated_time());
-        this.route_info = new SimpleStringProperty(tour.getRoute_info());
-
+    public ObservableList<TourViewModel> getData() {
+        return data;
     }
 
-    public TourTableViewModel(TourTableViewModel ttvm) {
-        this.id = new SimpleStringProperty(ttvm.getId());
-        this.name = new SimpleStringProperty(ttvm.getName());
-        this.tour_description = new SimpleStringProperty(ttvm.getTour_description());
-        this.from = new SimpleStringProperty(ttvm.getFrom());
-        this.to = new SimpleStringProperty(ttvm.getTo());
-        this.transport_type = new SimpleStringProperty(ttvm.getTransport_type());
-        this.tour_distance = new SimpleIntegerProperty(ttvm.getTour_distance());
-        this.estimated_time = new SimpleIntegerProperty(ttvm.getEstimated_time());
-        this.route_info = new SimpleStringProperty(ttvm.getRoute_info());
+    public void newTour(TourViewModel svm) {
+        data.add(svm);
     }
 
-    public String getId() {
-        return id.get();
+    public void updateTour(TourViewModel studentViewModel) {
+        for (TourViewModel tour : data) {
+            if (tour.getId().equals(studentViewModel.getId())) {
+                tour.nameProperty().set(studentViewModel.nameProperty().get());
+                tour.tour_distanceProperty().set(studentViewModel.tour_distanceProperty().get());
+                tour.fromProperty().set(studentViewModel.fromProperty().get());
+                tour.toProperty().set(studentViewModel.toProperty().get());
+                tour.transport_typeProperty().set(studentViewModel.transport_typeProperty().get());
+                tour.tour_distanceProperty().set(studentViewModel.tour_distanceProperty().get());
+                tour.estimated_timeProperty().set(studentViewModel.estimated_timeProperty().get());
+                tour.route_infoProperty().set(studentViewModel.route_infoProperty().get());
+            }
+        }
     }
-    public String getName(){
-        return name.get();
-    }
-    public String getTour_description() {
-        return tour_description.get();
-    }
-    public String getFrom() {
-        return from.get();
-    }
-    public String getTo() {
-        return to.get();
-    }
-    public String getTransport_type() {
-        return transport_type.get();
-    }
-    public int getTour_distance() {
-        return tour_distance.get();
-    }
-    public int getEstimated_time() {
-        return estimated_time.get();
-    }
-    public String getRoute_info() {
-        return route_info.get();
-    }
-
-    public StringProperty idProperty() {
-        return id;
-    }
-    public StringProperty nameProperty() {
-        return name;
-    }
-    public StringProperty tour_descriptionProperty() {
-        return tour_description;
-    }
-    public StringProperty fromProperty() {
-        return from;
-    }
-    public StringProperty toProperty() {
-        return to;
-    }
-    public StringProperty transport_typeProperty() {
-        return transport_type;
-    }
-    public IntegerProperty tour_distanceProperty() {
-        return tour_distance;
-    }
-    public IntegerProperty estimated_timeProperty() {
-        return estimated_time;
-    }
-    public StringProperty route_infoProperty() {
-        return route_info;
+    public void deleteTour(TourViewModel studentViewModel) {
+        data.remove(studentViewModel);
+        // sonstiges??
     }
 }
