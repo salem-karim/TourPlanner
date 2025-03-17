e8f6f9e322d640353e13b49ddbafe03c66134f76
salem-karim <salem.karim@icloud.com>
2025-03-17 21:04:23 +0100

    feat: Add TestFX dependency and add polish i18n

--- src/test/java/TourFXTests.java
+++ src/test/java/TourFXTests.java
@@ -1,54 +1,54 @@
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.matcher.control.LabeledMatchers.hasText;
-
-import at.technikum.tourplanner.controllers.TourPlannerController;
-import at.technikum.tourplanner.models.Tour;
-import at.technikum.tourplanner.viewmodels.TourTableViewModel;
-import at.technikum.tourplanner.viewmodels.TourViewModel;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit5.ApplicationTest;
-
-public class TourFXTests extends ApplicationTest {
-
-    private TourTableViewModel ttvm; // or TourViewModel??
-
-    // todo
-    @Override
-    public void start(Stage stage) {
-        ttvm = new TourTableViewModel();
-        TourPlannerController controller = new TourPlannerController(ttvm);
-        stage.setScene(controller.getScene());
-        stage.show();
-    }
-
-    // testing test
-    @Test
-    void testButtonClickUpdatesViewModel() {
-        clickOn("#myButton");  // Target button by ID
-        verifyThat("#myLabel", hasText("Updated!"));  // Verify the label updated
-    }
-
-    @Test
-    void testButtonClickOpensCreateWindow() {
-
-    }
-
-    @Test
-    void testButtonClickOpensEditWindow() {
-
-    }
-
-    @Test
-    void testButtonClickOpensLogsWindow() {
-
-    }
-
-    @Test
-    void testButtonClickOpensGeneralWindow() {
-
-    }
-}
+//import static org.testfx.api.FxAssert.verifyThat;
+//import static org.testfx.matcher.control.LabeledMatchers.hasText;
+//
+//import at.technikum.tourplanner.controllers.TourPlannerController;
+//import at.technikum.tourplanner.models.Tour;
+//import at.technikum.tourplanner.viewmodels.TourTableViewModel;
+//import at.technikum.tourplanner.viewmodels.TourViewModel;
+//import javafx.scene.control.Button;
+//import javafx.scene.layout.StackPane;
+//import javafx.stage.Stage;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.testfx.framework.junit5.ApplicationTest;
+//
+//public class TourFXTests extends ApplicationTest {
+//
+//    private TourTableViewModel ttvm; // or TourViewModel??
+//
+//    // todo
+//    @Override
+//    public void start(Stage stage) {
+//        ttvm = new TourTableViewModel();
+//        TourPlannerController controller = new TourPlannerController(ttvm);
+//        stage.setScene(controller.getScene());
+//        stage.show();
+//    }
+//
+//    // testing test
+//    @Test
+//    void testButtonClickUpdatesViewModel() {
+//        clickOn("#myButton");  // Target button by ID
+//        verifyThat("#myLabel", hasText("Updated!"));  // Verify the label updated
+//    }
+//
+//    @Test
+//    void testButtonClickOpensCreateWindow() {
+//
+//    }
+//
+//    @Test
+//    void testButtonClickOpensEditWindow() {
+//
+//    }
+//
+//    @Test
+//    void testButtonClickOpensLogsWindow() {
+//
+//    }
+//
+//    @Test
+//    void testButtonClickOpensGeneralWindow() {
+//
+//    }
+//}
